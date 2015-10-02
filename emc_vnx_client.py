5b88854e347f87f80c56221a4555d07a02fdf71f
Madhuri Yechuri <madhuri.yechuri@clusterhq.com>
2015-10-02 14:24:53 -0700

    Fix emc_vnx_client.

--- emc_vnx_client.py
+++ emc_vnx_client.py
@@ -1,227 +1,244 @@
-
-from flocker.node.agents.blockdevice import (
-    VolumeException, AlreadyAttachedVolume,
-    UnknownVolume, UnattachedVolume,
-    IBlockDeviceAPI, _blockdevicevolume_from_dataset_id,
-    _blockdevicevolume_from_blockdevice_id
-)
-
-from eliot import Message, Logger
-from twisted.python.filepath import FilePath
-from zope.interface import implementer
-from subprocess import check_output
-
-import base64
-import urllib
-import urllib2
-import json
+import multiprocessing
 import os
-import random
 import re
-import socket
+import time
 
-from emc_vnx_client import EMCVNXClient
-
-
-_logger = Logger()
-
-
-def vnx_api(cluster_id, user, password, ip, pool):
-    return EMCVNXBlockAPI(cluster_id, user, password, ip, pool)
-
-def rescan_iscsi(number=None):
-    cmd = ["rescan-scsi-bus", "-r", "-c"]
-    if number:
-       cmd.append(str(number))
-    check_output(["rescan-scsi-bus", "-r", "-c"])
-
-def get_iqn():
-    out = check_output(["cat", "/etc/iscsi/initiatorname.iscsi"])
-    return out.split('\n')[-2].split('=')[-1]
+try:
+    import eventlet
+    from eventlet.green import subprocess
+except:
+    import subprocess
 
 
-@implementer(IBlockDeviceAPI)
-class EMCVNXBlockAPI(object):
+CLI_PATH = '/opt/Navisphere/bin/naviseccli'
 
-    VERSION = '0.1'
-    driver_name = 'VNX'
 
-    def __init__(self, cluster_id, user, password, ip, pool):
-        self._client = EMCVNXClient(user, password, ip)
-        self._cluster_id = cluster_id
-        self._pool = pool
-        self._hostname = socket.gethostname()
-        self._setup()
+def execute(*cmd):
+    try:
+        _PIPE = subprocess.PIPE
+        obj = subprocess.Popen(
+            cmd, stdin=_PIPE, stdout=_PIPE, stderr=_PIPE,
+            close_fds=True, shell=False)
+        out, err = obj.communicate()
+        obj.stdin.close()
+        _returncode = obj.returncode
+        return _returncode, out, err
+    finally:
+        time.sleep(0)
 
-    def _setup(self):
-        #print('New EMC VNX flocker driver setup')
-        Message.new(info=u'Entering EMC VNX _setup').write(_logger)
-        if not self._client.check_pool(self._pool):
-             raise Exception('The pool does not exist')
-        self.iscsi_targets = self._client.get_iscsi_targets()
 
-    def _convert_volume_size(self, size):
-        """
-        convert KB to GB
-        """
-        return size/(1024*1024*1024)
+class PropertyDescriptor(object):
+    def __init__(self, option, label, key, converter=None):
+        self.option = option
+        self.label = label
+        self.key = key
+        self.converter = converter
 
-    def create_volume(self, dataset_id, size):
-        Message.new(info=u'Entering EMC VNX create_volume').write(_logger)
-        volume = _blockdevicevolume_from_dataset_id(
-            size=size, dataset_id=dataset_id)
-        rc, out = self._client.create_volume(
-            str(volume.blockdevice_id),
-            str(self._convert_volume_size(size)),
-            self._pool)
-        if rc != 0 and out.find('Unable to create the LUN because the specified name is already in use') == -1: 
-            raise Exception(out)
-        return volume
 
-    def destroy_volume(self, blockdevice_id):
-        Message.new(info=u'Entering EMC VNX destroy_volume',
-                    blockdevice_id=blockdevice_id).write(_logger)
-        self.destroy_volume(str(blockdevice_id))
+class EMCVNXClient(object):
 
-    def attach_volume(self, blockdevice_id, attach_to):
-        Message.new(info=u'Entering EMC VNX attach_volume',
-                    blockdevice_id=blockdevice_id,
-                    attach_to=attach_to).write(_logger)
-        self._client.create_storage_group(str(attach_to))
-        self._client.connect_host_to_sg(str(attach_to), str(attach_to))
-        lun = self._client.get_lun_by_name(blockdevice_id)
-        alu = lun['lun_id']
-        hlu = self.choose_hlu(str(attach_to))
-        self._client.add_volume_to_sg(str(hlu), str(alu), str(attach_to))
-        volume = _blockdevicevolume_from_blockdevice_id(
-            blockdevice_id=blockdevice_id,
-            size=int(lun['total_capacity_gb']*1024*1024*1024),
-            attached_to=unicode(attach_to)
-        )
-        return volume
-        
-    def resize_volume(self, blockdevice_id, size):
-        Message.new(info=u'Entering EMC VNX resize_volume',
-                    blockdevice_id=blockdevice_id).write(_logger)
+    POOL_NAME = PropertyDescriptor(
+        '-name',
+        'Pool Name:\s*(.*)\s*',
+        'pool_name')
 
-    def detach_volume(self, blockdevice_id):
-        Message.new(info=u'Entering EMC VNX detach_volume',
-                    blockdevice_id=blockdevice_id).write(_logger)
-        alu = self._client.get_lun_by_name(blockdevice_id)['lun_id'] 
-        rc, out = self._client.get_storage_group(self._hostname)
-        if rc != 0:
-             raise Exception('SG does not exist')
-        hlu = self._client.parse_sg_content(out)['lunmap'][alu]
-        self._client.remove_volume_from_sg(str(hlu), self._hostname)
-        rescan_iscsi(hlu)
+    LUN_STATE = PropertyDescriptor(
+        '-state',
+        'Current State:\s*(.*)\s*',
+        'state')
+    LUN_STATUS = PropertyDescriptor(
+        '-status',
+        'Status:\s*(.*)\s*',
+        'status')
+    LUN_NAME = PropertyDescriptor(
+        '-name',
+        'Name:\s*(.*)\s*',
+        'lun_name')
+    LUN_CAPACITY = PropertyDescriptor(
+        '-userCap',
+        'User Capacity \(GBs\):\s*(.*)\s*',
+        'total_capacity_gb',
+        float)
+    LUN_ID = PropertyDescriptor(
+        '-id',
+        'LOGICAL UNIT NUMBER\s*(\d+)\s*',
+        'lun_id',
+        int)
+    LUN_ALL = [LUN_STATE, LUN_STATUS, LUN_NAME, LUN_CAPACITY, LUN_ID]
 
-    def list_volumes(self):
-        Message.new(info=u'Entering EMC VNX list_volumes').write(_logger)
-        fake_vol = _blockdevicevolume_from_blockdevice_id(
-            blockdevice_id=u'block-886ed03a-5606-453a-94a9-a1cbaf35164c',
-            size=1024*1024*1024,
-            attached_to=u'f_host')
-        volumes = []
-        volumes.append(fake_vol)
+    def __init__(self, user, password, ip):
+        self.user = user
+        self.pwd = password
+        self.ip = ip
+        self.cli = (CLI_PATH, '-h', self.ip, '-user',
+                    self.user, '-password', self.pwd,
+                    '-scope', '0')
 
-        # get lun_map of this node
-        rc, out = self._client.get_storage_group(self._hostname)
-        if rc != 0:
-             raise Exception('SG does not exist')
-        lun_map = self._client.parse_sg_content(out)['lunmap']
-        
-        # add luns which belong to flocker
-        luns = self._client.get_all_luns()
-        for each in luns:
-            if each['lun_name'].startswith('block-'):
-                attached_to = None
-                if lun_map.has_key(each['lun_id']):
-                    attached_to = unicode(self._hostname)
-                vol = _blockdevicevolume_from_blockdevice_id(
-                    blockdevice_id=unicode(each['lun_name']),
-                    size=int(1024*1024*1024*each['total_capacity_gb']),
-                    attached_to=attached_to)
-                volumes.append(vol)
-        return volumes
+    def check_pool(self, name):
+        cmd = self.cli + ('storagepool', '-list', '-name', name)
+        data = self._get_obj_props(cmd, [self.POOL_NAME])
+        return data != {}
 
-    def get_device_path(self, blockdevice_id):
-        Message.new(info=u'Entering EMC VNX get_device_path',
-                    blockdevice_id=blockdevice_id).write(_logger)
-        lun = self._client.get_lun_by_name(blockdevice_id)
-        rc, out = self._client.get_storage_group(self._hostname)
-        if rc != 0:
-             raise Exception('SG does not exist')
-        lun_map = self._client.parse_sg_content(out)['lunmap']
-        hlu = lun_map[lun['lun_id']]
-        portals = self.get_iscsi_target_portals(get_iqn(),
-                                                self._hostname)
-        device_name = "ip-%s:3260-iscsi-%s-lun-%s" % (portals[0]['IP Address'],
-                                                      portals[0]['Port WWN'],
-                                                      str(hlu))
-        device = '/dev/disk/by-path/%s' % device_name
-        if self.discover_device(device):
-            return FilePath(device).realpath()
-        else:
-            raise Exception('Device not found')
+    def get_lun_by_name(self, name):
+        cmd = self.cli + ('lun', '-list', '-name', name)
+        return self._get_obj_props(cmd, self.LUN_ALL)
 
-    def discover_device(self, device):
-        a = 5
-        tries = 0
-        while tries < a:
-            tries = tries + 1
-            if os.path.exists(device):
-                return True
+    def get_all_luns(self):
+        cmd = self.cli + ('lun', '-list')
+        rc, out, err = execute(*cmd)
+        luns = []
+        if rc == 0:
+            raw_luns = out.split('\n\n')
+            for each in raw_luns:
+                if each == '':
+                    continue
+                lun = {
+                    prop.key:
+                        self._get_prop_value(each, prop)
+                    for prop in self.LUN_ALL
+                }
+                luns.append(lun) 
+        return luns
+
+    def _get_obj_props(self, cmd, props):
+        rc, out, err = execute(*cmd)
+        data = {}
+        if rc == 0:
+            data = {
+                prop.key:
+                    self._get_prop_value(out, prop)
+                for prop in props
+            }
+        return data
+
+    def _get_prop_value(self, out, prop):
+        label = prop.label
+        m = re.search(label, out)
+        if m:
+            if (prop.converter is not None):
+                try:
+                    return prop.converter(m.group(1))
+                except ValueError:
+                    return None
             else:
-                rescan_iscsi() 
-        return False 
- 
-    def allocation_unit(self):
-        Message.new(info=u'Entering EMC VNX allocation_unit').write(_logger)
-        return 1
+                return m.group(1)
+        else:
+            return None
 
-    def compute_instance_id(self):
-        Message.new(info=u'Entering EMC VNX compute_instance_id',
-                    hostanme=self._hostname).write(_logger)
-        return self._hostname
+    def create_volume(self, name, size, pool):
+        cmd = ('lun', '-create', '-capacity', size, '-sq', 'gb',
+               '-poolName', pool, '-name', name)
+        cmd = self.cli + cmd
+        rc, out, err = execute(*cmd)
+        return rc, out
 
-    def choose_hlu(self, sg_name):
-        rc, out = self._client.get_storage_group(sg_name)
+    def wait_for_volume(self, name, timeout=300):
+        start = time.time()
+        while time.time() - start < timeout:
+            lun = self.get_lun_by_name(name)
+            if not lun:
+                return
+            elif lun['state'] in ['Ready', 'Faulted']:
+                return
+        raise Exception('Timeout when waiting for a volume')
+
+    def destroy_volume(self, name):
+        cmd = ('lun', '-destroy', '-name', name,
+               '-forceDetach', '-o')
+        rc, out, err = execute(*(self.cli + cmd))
+        return rc, out
+
+    def create_storage_group(self, name):
+        cmd = ('storagegroup', '-create', '-gname', name)
+        rc, out, err = execute(*(self.cli + cmd))
+        return rc, out
+
+    def get_storage_group(self, name):
+        cmd = ('storagegroup', '-list', '-gname', name,
+               '-host', '-iscsiAttributes')
+        rc, out, err = execute(*(self.cli + cmd))
+        return rc, out
+
+    def parse_sg_content(self, content):
+        lun_map = {}
+        data = {'storage_group_uid': None,
+                'lunmap': lun_map,
+                'raw_output': ''}
+        data['raw_output'] = content
+        re_stroage_group_id = 'Storage Group UID:\s*(.*)\s*'
+        m = re.search(re_stroage_group_id, content)
+        if m is not None:
+            data['storage_group_uid'] = m.group(1)
+
+        re_hlu_alu_pair = 'HLU\/ALU Pairs:\s*HLU Number' \
+                          '\s*ALU Number\s*[-\s]*(?P<lun_details>(\d+\s*)+)'
+        m = re.search(re_hlu_alu_pair, content)
+        if m is not None:
+            lun_details = m.group('lun_details').strip()
+            values = re.split('\s*', lun_details)
+            while (len(values) >= 2):
+                key = values.pop()
+                value = values.pop()
+                lun_map[int(key)] = int(value)
+        return data
+
+    def add_volume_to_sg(self, hlu, alu, sg_name):
+        cmd = ('storagegroup', '-addhlu', '-hlu', hlu,
+               '-alu', alu, '-gname', sg_name, '-o')
+        rc, out, err = execute(*(self.cli + cmd))
+        return rc, out
+
+    def remove_volume_from_sg(self, hlu, sg_name):
+        cmd = ('storagegroup', '-removehlu', '-hlu', hlu,
+               '-gname', sg_name, '-o')
+        rc, out, err = execute(*(self.cli + cmd))
+        return rc, out
+
+    def connect_host_to_sg(self, host, sg_name):
+        cmd = ('storagegroup', '-connecthost', '-host', host,
+               '-gname', sg_name, '-o')
+        rc, out, err = execute(*(self.cli + cmd))
+        return rc, out
+
+    def get_iscsi_targets(self):
+        cmd = ('connection', '-getport', '-address', '-vlanid')
+        rc, out, err = execute(*(self.cli + cmd))
         if rc != 0:
-             raise Exception('SG does not exist')
-        lun_map = self._client.parse_sg_content(out)['lunmap']
-        candicates = list(set(range(1, 256)) - set(lun_map.values()))
-        return candicates[random.randint(0, len(candicates)-1)] 
-
-    def get_iscsi_target_portals(self, initiator, sg_name):
-        rc, out = self._client.get_storage_group(sg_name)
-        if rc != 0:
-             raise Exception('SG does not exist')
-        spport_set = set()
-        for m_spport in re.finditer(
-                r'\n\s+%s\s+SP\s.*\n.*\n\s*SPPort:\s+(A|B)-(\d+)v(\d+)\s*\n'
-                % initiator, out, flags=re.IGNORECASE):
-            spport_set.add((m_spport.group(1), int(m_spport.group(2)),
-                           int(m_spport.group(3))))
-
-        target_portals = []
-        all_portals = self.iscsi_targets['A'] + self.iscsi_targets['B']
-        random.shuffle(all_portals)
-        for portal in all_portals:
-            spport = (portal['SP'],
-                      portal['Port ID'],
-                      portal['Virtual Port ID'])
-            if spport not in spport_set:
+            raise Exception("Get port failed")
+        iscsi_target_dict = {'A': [], 'B': []}
+        iscsi_spport_pat = r'(A|B)\s*' + \
+                           r'Port ID:\s+(\d+)\s*' + \
+                           r'Port WWN:\s+(iqn\S+)'
+        iscsi_vport_pat = r'Virtual Port ID:\s+(\d+)\s*' + \
+                          r'VLAN ID:\s*\S*\s*' + \
+                          r'IP Address:\s+(\S+)'
+        for spport_content in re.split(r'^SP:\s+|\nSP:\s*', out):
+            m_spport = re.match(iscsi_spport_pat, spport_content,
+                                flags=re.IGNORECASE)
+            if not m_spport:
                 continue
-            target_portals.append(portal)
-        return target_portals
+            sp = m_spport.group(1)
+            port_id = int(m_spport.group(2))
+            iqn = m_spport.group(3)
+            for m_vport in re.finditer(iscsi_vport_pat, spport_content):
+                vport_id = int(m_vport.group(1))
+                ip_addr = m_vport.group(2)
+                if ip_addr.find('N/A') != -1:
+                    continue
+                iscsi_target_dict[sp].append({'SP': sp,
+                                              'Port ID': port_id,
+                                              'Port WWN': iqn,
+                                              'Virtual Port ID': vport_id,
+                                              'IP Address': ip_addr})
+        return iscsi_target_dict
 
 
 if __name__ == '__main__':
-    api = EMCVNXBlockAPI(None, 'sysadmin', 'sysadmin', '192.168.1.94', 'Pool_1')
+    cli = EMCVNXClient('sysadmin', 'sysadmin', '192.168.1.94')
+    #cli.check_pool('Pool_1')
+    #if cli.create_volume('flocker-001', '1', 'Pool_1'):
+    #    cli.wait_for_volume('flocker-001')
     import pdb;pdb.set_trace()
-    #volume = api.attach_volume('flocker-test-02', api._hostname)
-    
-    #api.get_device_path('flocker-test-02')
-    vols = api.list_volumes()
-    print vols
+    luns = cli.get_all_luns()
+    print luns
 
